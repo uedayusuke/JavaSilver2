c64c47d8eb5b093cb720ec276565c88afa263086
uedayusuke <uedayusuke1107@gmail.com>
2021-01-12 12:12:45 +0900

    [ADD]chap2:リテラル

---
+++ chap2/chap2/Ex1.java
@@ -0,0 +1,16 @@
+package chap2;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		System.out.println(255);
+		System.out.println(0b11111111);
+		System.out.println(0377);
+		System.out.println(0xff);
+		System.out.println(12.33);
+		System.out.println(3e4);
+		System.out.println('A');
+		System.out.println('\u3012');
+		System.out.println("Hello");
+		System.out.println(true);
+	}
+}
