571724aa1109c5b9540b6b0c6db4e713aa4310ce
uedayusuke <uedayusuke1107@gmail.com>
2021-01-12 20:44:02 +0900

    [ADD]chap2:変数

---
+++ chap2/chap2/Ex3.java
@@ -0,0 +1,23 @@
+package chap2;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = 20;
+		int i = 30;
+		long l = 40l;
+		float f = 5.23f;
+		double d = 3.14;
+		char c = 'c';
+		boolean bl = true;
+		
+		System.out.println("byte	:" + b);
+		System.out.println("short	:" + s);
+		System.out.println("int	:" + i);
+		System.out.println("long	:" + l);
+		System.out.println("float	:" + f);
+		System.out.println("double	:" + d);
+		System.out.println("char	:" + c);
+		System.out.println("boolean	:" + bl);
+	}
+}
