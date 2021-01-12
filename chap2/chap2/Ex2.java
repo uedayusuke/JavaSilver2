8cb5487bcf76307f528f472b9eb496f0a3006124
uedayusuke <uedayusuke1107@gmail.com>
2021-01-12 12:39:26 +0900

    [ADD]chap2:アンスコ

---
+++ chap2/chap2/Ex2.java
@@ -0,0 +1,21 @@
+package chap2;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		//float x1 = 3_.1415F;
+		//float x2 = 3._1415F;
+		//long x3 = 999_99_9999_L;
+		//int x4 = _56;
+		int x5 = 100_000;
+		System.out.println(x5);
+		//int x6 = 45_;
+		int x7 = 5______2;
+		System.out.println(x7);
+		//int x8 = 0_x78;
+		//int x9 = 0x_34;
+		int x10 = 0x5_1;
+		int x11 = 0_56;
+		System.out.println(x10);
+		System.out.println(x11);
+	}
+}
