1691a552251bc4d161e9e6b17eb498e9aeb6c22e
uedayusuke <uedayusuke1107@gmail.com>
2021-01-12 20:52:16 +0900

    [ADD]chap2:変数の初期化

---
+++ chap2/chap2/Ex4.java
@@ -0,0 +1,19 @@
+package chap2;
+
+public class Ex4 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		long num2 = 30;
+		
+		//long num3 = 1000000000000000;
+		long num3 = 1000000000000000l;
+		
+		//float num4 = 10.0;
+		float num4 = 10.0f;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+}
