d9065636d7d21da125ede8e998cae7c248dc8cdd
uedayusuke <uedayusuke1107@gmail.com>
2021-01-13 20:07:32 +0900

    [ADD]chap2:スコープ

--- chap2/chap2/Ex7.java
+++ chap2/chap2/Ex7.java
@@ -3,13 +3,15 @@ package chap2;
 public class Ex7 {
 	public static void main(String[] args) {
 		int x = 100;
+		System.out.println(x);
 		x = 200;
+		System.out.println(x);
 	}
 	
 	public static void method() {
 		/*
+		 * "x"を宣言したブロックを外れているため使用不可
 		 * x = 300;
 		 */
-		
 	}
 }
