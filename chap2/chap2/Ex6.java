76013d6a77a5d83dd266cc957df5fa6375ffe736
uedayusuke <uedayusuke1107@gmail.com>
2021-01-13 20:01:11 +0900

    [ADD]chap2:スコープ

--- chap2/chap2/Ex6.java
+++ chap2/chap2/Ex6.java
@@ -4,10 +4,16 @@ public class Ex6 {
 	public static void main(String[] args) {
 		{
 			int x = 100;
+			System.out.println(x);
 			x = 200;
+			System.out.println(x);
 		}
-		//System.out.println(x);
-		//x = 300;
+		
+		/*
+		ブロックを外れている目、"x"は使えない
+		System.out.println(x);
+		x = 300;		
+		*/
 		
 	}
 }
