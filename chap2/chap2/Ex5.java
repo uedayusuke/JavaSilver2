6138671d2cb383320b9a0481413ca819cc85fe34
uedayusuke <uedayusuke1107@gmail.com>
2021-01-13 19:48:50 +0900

    [ADD]chap2:定数

---
+++ chap2/chap2/Ex5.java
@@ -0,0 +1,15 @@
+package chap2;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		num1 = 20;
+		final int num2 = 30;
+		/*
+		 * "final"修飾子で定数になるため、新たに代入できない
+		 * num2 = 40;
+		 */
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+}
