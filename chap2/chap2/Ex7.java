db26180f351943773ff12719efe778ea81c0c741
uedayusuke <uedayusuke1107@gmail.com>
2021-01-13 20:05:21 +0900

    [ADD]chap2:スコープ

---
+++ chap2/chap2/Ex7.java
@@ -0,0 +1,15 @@
+package chap2;
+
+public class Ex7 {
+	public static void main(String[] args) {
+		int x = 100;
+		x = 200;
+	}
+	
+	public static void method() {
+		/*
+		 * x = 300;
+		 */
+		
+	}
+}
