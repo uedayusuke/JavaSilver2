039d692a2e834d277178fa20020bce23a021ef31
uedayusuke <uedayusuke1107@gmail.com>
2021-01-14 16:49:15 +0900

    [ADD]chap2:一次元配列

---
+++ chap2/chap2/Ex10.java
@@ -0,0 +1,12 @@
+package chap2;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		int i;
+		//System.out.println("iの値	:" + i);
+		
+		int[] array = new int[1];
+		System.out.println("array[0]:	" + array[0]);
+		System.out.println("array[1]:	" + array[1]);
+	}
+}
