c3e589b63b67e0ebdc9a666e219bb8309ea55487
uedayusuke <uedayusuke1107@gmail.com>
2021-01-14 16:04:38 +0900

    [ADD]chap2:var

---
+++ chap2/chap2/Ex8.java
@@ -0,0 +1,25 @@
+package chap2;
+
+public class Ex8 {	
+	/*
+	var a = 100;
+	static var b = 100;
+	*/
+	
+	public static void main(String[] args) {
+		var c = 100;
+		var d = "Hello WOrld!!";
+		/*
+		var e;
+		var f = null;
+		*/
+		final var g = 10;
+		/*
+		var final h = 10;
+		*/
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(g);
+	}
+}
