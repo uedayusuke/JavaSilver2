c66eabb3a6a634a8db608787e0c4039d53b53d48
uedayusuke <uedayusuke1107@gmail.com>
2021-01-14 16:52:51 +0900

    [ADD]chap2:一次元配列

--- chap2/chap2/Ex10.java
+++ chap2/chap2/Ex10.java
@@ -3,10 +3,18 @@ package chap2;
 public class Ex10 {
 	public static void main(String[] args) {
 		int i;
-		//System.out.println("iの値	:" + i);
+		/*
+		初期値を代入していないためコンパイルエラー
+		System.out.println("iの値	:" + i);
+		*/
 		
-		int[] array = new int[1];
+		int[] array = new int[2];
+		//初期値を代入していない場合、int型は"0"がデフォで代入される
 		System.out.println("array[0]:	" + array[0]);
 		System.out.println("array[1]:	" + array[1]);
+		/*
+		配列の要素数を超えている場合は実行時エラーとなる
+		System.out.println("array[2]:	" + array[2]);
+		*/
 	}
 }
