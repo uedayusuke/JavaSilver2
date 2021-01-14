f1ac648bc654e5f087a2b657fad740e6162476f3
uedayusuke <uedayusuke1107@gmail.com>
2021-01-14 16:06:56 +0900

    [ADD]chap2:var

--- chap2/chap2/Ex8.java
+++ chap2/chap2/Ex8.java
@@ -2,6 +2,7 @@ package chap2;
 
 public class Ex8 {	
 	/*
+	クラス直下のメンバ変数では使用不可
 	var a = 100;
 	static var b = 100;
 	*/
@@ -10,11 +11,13 @@ public class Ex8 {
 		var c = 100;
 		var d = "Hello WOrld!!";
 		/*
+		右辺に値を代入しないと型を推論できない
 		var e;
 		var f = null;
 		*/
 		final var g = 10;
 		/*
+		定数で宣言する場合、fainalは先に記述
 		var final h = 10;
 		*/
 		
