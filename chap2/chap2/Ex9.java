64a2ca141d21bbc1d4ef9cf301453ebba2fe935c
uedayusuke <uedayusuke1107@gmail.com>
2021-01-14 16:37:31 +0900

    [ADD]chap2:一次元配列

--- chap2/chap2/Ex9.java
+++ chap2/chap2/Ex9.java
@@ -2,15 +2,17 @@ package chap2;
 
 public class Ex9 {
 	public static void main(String[] args) {
-		char[] c;
-		c = new char[3];
-		int[] i = new int[3];
-		String[] str = {"Welcome", "to", "JAVA"};
+		char[] c; //char型の配列"c"を宣言
+		c = new char[3]; //配列"c"の要素数を定義
+		int[] i = new int[3]; //int型の配列"i"を宣言し、要素数を定義
+		String[] str = {"Welcome", "to", "JAVA"}; //String型の配列"str"を宣言し、初期値を代入
 		
+		//配列cに値を代入
 		c[0] = 'A';
 		c[1] = 'B';
 		c[2] = 'C';
 		
+		//配列iに値を代入
 		i[0] = 100;
 		i[1] = 200;
 		i[2] = 300;
